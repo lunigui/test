73426f82b760adf98e8b30848dc2de5498eae385
lunigui <2609574223@qq.com>
2025-05-25 21:21:50 +0800

    init commit

---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // 引入输入输出流库
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl; // 输出 Hello, World! 并换行
+    return 0;                                  // 返回 0 表示程序成功结束
+}
